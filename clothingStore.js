1bb8081dd7b344463f7f7be60bef7e78bbc357e7
Brwnber <kylesmall32@yahoo.com>
2017-12-04 21:13:27 -0500

    Added functions
    added removeFromCart, removeFromCartAll, clearCart, countCart, totalCart, listCart, saveCart, loadCart

--- clothingStore.js
+++ clothingStore.js
@@ -18,8 +18,8 @@ var item2 = {
 var inventory = [item1, item2];
 
 function addToCart(itemName){
-    var itemFound = false;
-    for (var i = 0; i < myShoppingCart.length;i++){
+    let itemFound = false;
+    for (let i = 0; i < myShoppingCart.length;i++){
         if (myShoppingCart[i].name === itemName){
             myShoppingCart[i].quantity++;
             itemFound = true;
@@ -28,10 +28,78 @@ function addToCart(itemName){
     }
 
     if (!itemFound){
-        for (var i = 0; i < inventory.length;i++){
+        for (let i = 0; i < inventory.length;i++){
             if(inventory[i].name === itemName){
                 myShoppingCart.push(inventory[i]);
             }
         }
     }
+    saveCart();
 }
+
+function removeFromCart(itemName){
+    for (let i in myShoppingCart) {
+        if(myShoppingCart[i].name === itemName){
+            myShoppingCart[i].quantity--;
+            if(myShoppingCart[i].quantity === 0) {
+                myShoppingCart.splice(i, 1);
+            }
+            break;
+        }
+    }
+    saveCart();
+}
+
+function removeFromCartAll(itemName){
+    for (let i in myShoppingCart) {
+        if (myShoppingCart[i].name === itemName) {
+            cart.splice(i, 1);
+            break;
+        }
+    }
+    saveCart();
+}
+
+function clearCart() {
+    myShoppingCart = [];
+    saveCart();
+}
+
+function countCart() {
+    let totalCount = 0;
+    for (let i in myShoppingCart) {
+        totalcount += myShoppingCart[i].quantity;
+    }
+    return totalCount;
+}
+
+function totalCart() {
+    let totalCost = 0;
+    for (let i in myShoppingCart) {
+        totalCost += myShoppingCart[i].price;
+    }
+    return totalCost;
+}
+
+function listCart(){
+    let cartCopy = [];
+    for (let i in myShoppingCart){
+        let item = myShoppingCart[i];
+        let itemCopy = {};
+        for (let p in item) {
+            itemCopy[p] = item[p];
+        }
+        cartCopy.push(itemCopy);
+    }
+    return cartCopy;
+}
+
+function saveCart() {
+    localStorage.setItem("shoppingCart", JSON.stringify(myShoppingCart));
+}
+
+function loadCart() {
+    myShoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
+}
+
+loadCart();
